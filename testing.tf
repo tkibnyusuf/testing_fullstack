682f709e9ae0f1410d67fc28544a8f9c2bee1593
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-22 17:05:04 -0500

    Update testing.tf

--- testing.tf
+++ testing.tf
@@ -2,18 +2,43 @@ provider "aws" {
   region = "us-east-1"  # Specify your AWS region
 }
 
-# Reference the existing S3 bucket
-data "aws_s3_bucket" "app_bucket" {
-  bucket = "my-app-devel-bucket"  # Replace with your existing bucket name
+# Create a remote backend for your terraform 
+terraform {
+  backend "s3" {
+    bucket = "yusuf-docker-tfstate"
+    dynamodb_table = "app-state"
+    key    = "LockID"
+    region = "us-east-1"
+    profile = "yusuf"
+  }
+}
+variable "environment_name" {
+  description = "Environment to deploy: devel, stage, prod"
+  type        = string
+}
+
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "my-app-${var.environment}-bucket"
+  acl    = "public-read"
+
+  versioning {
+    enabled = true
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
 resource "aws_s3_bucket_object" "build_files" {
   for_each = fileset("/home/runner/work/testing_fullstack/testing_fullstack/build", "**")
 
-  bucket = data.aws_s3_bucket.app_bucket.bucket
+  bucket = aws_s3_bucket.app_bucket.bucket
   key    = each.value
   source = "/home/runner/work/testing_fullstack/testing_fullstack/build/${each.value}"
   acl    = "public-read"
   }
 output "bucket_url" {
-  value = data.aws_s3_bucket.app_bucket.website_endpoint
+  value = aws_s3_bucket.app_bucket.website_endpoint
 }
