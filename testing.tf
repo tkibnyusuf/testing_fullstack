351d638dea04b4cbb6a6ae61e893c8a43c9bddbb
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-22 17:58:04 -0500

    Update testing.tf

--- testing.tf
+++ testing.tf
@@ -9,6 +9,8 @@ terraform {
     dynamodb_table = "app-state"
     key    = "LockID"
     region = "us-east-1"
+    aws_access_key_id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+    aws_secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
   }
 }
 
