e6c131ea2767146ec6b43fce3b73c61c7cb4d648
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-22 17:08:17 -0500

    Update testing.tf

--- testing.tf
+++ testing.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"  # Specify your AWS region
 }
 
+
 # Reference the existing S3 bucket
 data "aws_s3_bucket" "app_bucket" {
   bucket = "my-app-devel-bucket"  # Replace with your existing bucket name
