e11a94d1929c0836fd9ab265cee749ff14531655
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-22 14:19:30 -0500

    Update testing.tf

--- testing.tf
+++ testing.tf
@@ -15,6 +15,18 @@ resource "aws_s3_bucket_object" "build_files" {
   key    = each.value
   source = "${path.module}/build/${each.value}"
   acl    = "public-read"  # Adjust according to your needs
+  # Set content-type based on file extension
+  content_type = lookup(
+    {
+      ".html" = "text/html",
+      ".css"  = "text/css",
+      ".js"   = "application/javascript",
+      ".json" = "application/json",
+      ".png"  = "image/png",
+      ".jpg"  = "image/jpeg"
+    },
+    substr(each.value, length(each.value) - 5, 5), # Get the last part of the file name (extension)
+    "applicati
 
 }
 
