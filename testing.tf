a0359785ca0cb3d3ce07ee03f999bb85c964c720
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-22 19:42:57 -0500

    Update testing.tf

--- testing.tf
+++ testing.tf
@@ -48,6 +48,7 @@ resource "aws_s3_bucket_policy" "public_read_policy" {
         Effect = "Allow"
         Principal = "*"
         "Action": [
+         "s3:PutBucketPolicy",
          "s3:GetObject"
       ],
 
