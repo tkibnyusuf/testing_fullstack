954b730eedf7cfb3e04f61b3722364d88d74a567
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-22 20:37:35 -0500

    Update testing.tf

--- testing.tf
+++ testing.tf
@@ -47,6 +47,19 @@ resource "aws_s3_bucket_object" "build_files" {
   key    = each.value
   source = "/home/runner/work/testing_fullstack/testing_fullstack/build/${each.value}"
   acl    = "public-read"
+ # Set content-type based on file extension
+  content_type = lookup(
+    {
+      ".html" = "text/html",
+      ".css"  = "text/css",
+      ".js"   = "application/javascript",
+      ".json" = "application/json",
+      ".png"  = "image/png",
+      ".jpg"  = "image/jpeg"
+    },
+    substr(each.value, length(each.value) - 5, 5),
+    "application/octet-stream" # Default if not matched
+  )
   }
 output "bucket_url" {
   value = aws_s3_bucket.app_bucket.website_endpoint
